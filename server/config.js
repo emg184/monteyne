b92a8ab1359c7ae0c98f54da3d57f51ece8e977e
emg184 <gardner.ethan@comcast.net>
2018-05-13 21:04:11 -0400

    Delete config.js

--- server/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  secret: 'asdfasdfasdfasdfasdfadffasdfasdfasdfasdfa',
-  email: 'customapparelapi@gmail.com',
-  ULTRASECRETPASS: 'tbegcview',
-  accessKeyId: 'AKIAJSZTSUD2QTGR772Q',
-  secretAccessKey: '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'
-}
